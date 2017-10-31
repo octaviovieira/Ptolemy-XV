4757c6592f287c0a888f1c03876a32ba54a93072
João Victor Guinelli <jvguinelli@gmail.com>
2017-10-31 03:44:21 -0200

    Adição do arquivo aes.h

--- aes/teste_aes.c
+++ aes/teste_aes.c
@@ -1,39 +1,22 @@
 #include <stdlib.h>
 #include <string.h>
-#include "./rijndael-api-fst.c"
-#include "./rijndael-api-fst.h"
-#include "./rijndael-alg-fst.c"
-#include "./rijndael-alg-fst.h"
-
+#include "./aes.h"
 int main(int argc, char* argv[])
 {
-    cipherInstance *cipher	= malloc(sizeof(cipherInstance));
-    keyInstance *key 		= malloc(sizeof(keyInstance));
-    BYTE *input 		= malloc(sizeof(BYTE[16]));
-    BYTE *outBuffer 		= malloc(sizeof(BYTE[16]));
-    int inputLength 		= 128;
+	
+    char* text = "00000000000000000000000000000000";
+    char* key = "80000000000000000000000000000000";
+    BYTE* ct = ptolemy_aes_enc(text, key);
 
-    strcpy(input, "00000000000000000000000000000000");
-
-    strcpy(key->keyMaterial, "40000000000000000000000000000000");    
-    key->keyLen 		= 128;
-    key->direction 		= DIR_ENCRYPT;
     
-    cipher->mode 		= MODE_ECB;
-
-    makeKey(key, DIR_ENCRYPT, inputLength, key->keyMaterial);
-    
-    blockEncrypt(cipher, key, input, inputLength, outBuffer);
-    
-    printf("TEXT: %s\n", input);
-    printf("KEY:  %s\n", key->keyMaterial);
-    printf("OUT:  %s\n", outBuffer);
+    printf("TEXT: %s\n", text);
+    printf("KEY:  %s\n", key);
+    printf("OUT:  %s\n", ct);
     printf("OUT: ");
-    while(*outBuffer)
+    while(*ct)
     {
-        printf("%02x", *outBuffer);
-        ++outBuffer;
+        printf("%02x", *ct);
+        ++ct;
     }
-    return 0;
 }
 
